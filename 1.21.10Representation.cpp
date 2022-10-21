766b2e60bbb079d32e5eacc1d08f6f2f8dcccd05
Isha Tripathi <95401691+ishatri@users.noreply.github.com>
2022-10-21 10:44:24 +0530

    Create 1.21.10Representation.cpp

---
+++ 1.21.10Representation.cpp
@@ -0,0 +1,14 @@
+//no of nodes and edges will be given 
+//n = nodes
+//m = edges lines
+// 5 6 
+//1 2 or 2 1 
+//1 3
+// 2 4
+// 3 4
+// 25 
+// 4 5
+
+//storing as a list or a matrice
+// nodes can be zero based or 1 based
+
