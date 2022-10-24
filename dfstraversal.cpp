d594ad4828ebda42a4e617f5a8c57a78ef8d7fbc
Isha Tripathi <95401691+ishatri@users.noreply.github.com>
2022-10-24 10:28:29 +0530

    Create dfstraversal.cpp

---
+++ dfstraversal.cpp
@@ -0,0 +1,62 @@
+//we will initite a visited array which will keep a track of all of the visited nodes and look recursively at the neighbours
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    void dfs(int node, vector<int>adj[],int vis[], vector<int> &ans)
+    {
+        vis[node]=1;
+        ans.push_back(node);
+        for(auto it: adj[node])
+        {
+            if(!vis[it])
+            {
+                dfs(it,adj,vis,ans);
+            }
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        //here we have been given the number of nodes and the adj list
+        //starting idex is zero
+        vector<int> ans;
+        int start =0;
+        int vis[V]= {0};
+        dfs(start, adj,vis,ans);
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
